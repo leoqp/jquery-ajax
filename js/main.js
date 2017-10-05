77cf9f0a10db9e3c8a2e8a3694a60eb1c33fc640
Leonardo Pinheiro <leonardo.q.pinheiro@gmail.com>
2017-10-05 12:06:52 -0300

    Post e Atualizacao do Ajax

--- js/main.js
+++ js/main.js
@@ -8,7 +8,38 @@ var setConfig = function()
   $('#titulo-pagina').html(propriedades.titulo);
 };
 
+var testAjax = function(){
+
+  $('#AjaxRequest').submit(function(event){
+      //alert('ok');
+      //Não executar a ação padrão do formulário - submit
+      event.preventDefault();
+
+      //Faz a requisição para o servidor utilizando os parametros abaixo
+      var request = $.ajax({
+          method:"GET",
+          url:"post.php",
+          data:{
+              nome: $(':input[name=nome]').val(),
+              email:$(':input[name=email]').val(),
+              tel:$(':input[name=tel]').val()
+          }
+      });
+
+      //Aguarda a resposta da requisicao
+      request.always(function(e)
+      {
+        console.log(e);
+      });
+
+      //Não executar a ação padrão do formulário - submit -
+      //Usado o preventDefault pela acao
+      //return false;
+  });
+};
+
 $(document).ready(function() {
    // Stuff to do as soon as the DOM is ready
    setConfig();
+   testAjax();
 });
---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+  if($_GET)
+  {
+      var_dump($_GET);
+  }
+
+ ?>
